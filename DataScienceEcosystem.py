7865927e860ee881211ede839517202940d376df
Shitanshu Ghildiyal <87275651+ghildiyalshitanshu@users.noreply.github.com>
2023-08-12 19:39:18 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# #### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives**
+# ###### List popular languages for Data Science:
+# - R
+# - Python
+# - Julia
+# - Scala
+# - Java
+
+# ###### Some of the popular languages that Data Scientists use are:
+# 1. R 
+# 2. Scala
+# 3. Python
+
+# ###### Some of the commonly used libraries used by Data Scientists include:
+# 1. Keras
+# 2. TensorFlow
+# 3. Pandas
+# 4. Numpy
+# 5. ggplot
+
+# ###### Data Science Tools:
+# <table style="width:10%" align="left">
+# <tr>
+# <th>Data Science Tools</th>
+# </tr>
+# 
+# <tr>
+# <td>RStudio</td>
+# </tr>
+# 
+# <tr>
+# <td>Apache Spark</td>
+# </tr>
+#     
+# <tr>
+# <td>Apache Hadoop</td>
+# </tr>
+# 
+# </table>
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python:
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers.
+x = (3*4)+5
+print(x)
+
+
+# In[9]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+x = 200/60
+y=200%60
+print(int(x),"hours",y,"minutes")
+
+
+# ## Author
+# Shitanshu Ghildiyal
